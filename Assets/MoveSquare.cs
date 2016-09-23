6b95ffe8f657571304fccb2406c4816bf2606e17
Anthony Benavente <ambenavente1@catamount.wcu.edu>
2016-09-23 10:28:57 -0400

    Added Unity files

---
+++ Assets/MoveSquare.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveSquare : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position.Set(this.transform.position.x + 10, 
+			this.transform.position.y, this.transform.position.z);
+	}
+}
